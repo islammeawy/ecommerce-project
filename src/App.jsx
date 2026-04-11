e08a698c93ea9141ce2d40f8ace37c3f942a7eb1
Islam <i.mekawy.dev@gmail.com>
2026-04-11 13:20:49 +0200

    moved order page into react

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Home } from './pages/home'
 import { CheckoutPage } from './pages/CheckoutPage'
+import { Orders } from './pages/orders'
 import { Routes, Route } from 'react-router-dom'
 import './pages/home.css'
 import './pages/header.css'
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route index element={<Home />} />
       <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/orders" element={<Orders />} />
     </Routes>
   )
 }
