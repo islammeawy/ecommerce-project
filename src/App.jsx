0721d2d401b47e2c92b15f8eab183a9d9a69a962
Islam <i.mekawy.dev@gmail.com>
2026-04-11 13:08:07 +0200

    refactor: update routing to use react-router-dom and add CheckoutPage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import { Home } from './pages/home'
-import { Routes , Route } from 'react-router'
+import { CheckoutPage } from './pages/CheckoutPage'
+import { Routes, Route } from 'react-router-dom'
 import './pages/home.css'
 import './pages/header.css'
 
-
 function App() {
-  
-
   return (
     <Routes>
       <Route index element={<Home />} />
-      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
     </Routes>
-    
   )
 }
-
 
 export default App
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,6 +4,8 @@ export function Home() {
 
   return (
     <>
+      <title>Ecommerce project</title>
+
       <div className="header">
         <div className="left-section">
           <a href="index.html" className="header-link">
