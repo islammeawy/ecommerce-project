8be9b51caf2d5da5d57a1d0b6794d404f81d3296
Islam <i.mekawy.dev@gmail.com>
2026-04-13 13:40:27 +0200

    feat: integrate Axios for fetching products data in Home component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,18 @@
 import './home.css'
 import { Header } from '../components/Header';
-import { products } from '../../starting-code/data/products';
+import { useEffect,useState } from 'react';
+import axios from 'axios';
 
 export function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/products')
+      .then((response) => {
+        setProducts(response.data);
+      });
+  }, []);
+
 
   return (
     <>
